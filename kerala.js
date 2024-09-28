3e1eb11b8c4585ecc2ef4c1ea5efd6aa0d78e3be
jhilpatel06 <162791238+jhilpatel06@users.noreply.github.com>
2024-09-29 01:08:55 +0530

    Update kerala.js

--- kerala.js
+++ kerala.js
@@ -5,7 +5,7 @@ let userState = {
   budget: null,
 };
 
-function handleKeralaResponse(userMessage) {
+export function handleKeralaResponse(userMessage) {
   const cityList = ['kochi', 'munnar', 'alleppey'];
   
   // Step 1: If no city is selected yet, prompt the user to select one
