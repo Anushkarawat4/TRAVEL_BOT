1e7c74288c8a7ec09f029f28c684df54a2a997a1
jhilpatel06 <162791238+jhilpatel06@users.noreply.github.com>
2024-09-29 01:08:08 +0530

    Update himachal.js

--- himachal.js
+++ himachal.js
@@ -5,7 +5,7 @@ let userState = {
   budget: null,
 };
 
-function handleHimachalResponse(userMessage) {
+export function handleHimachalResponse(userMessage) {
   const cityList = ['shimla', 'manali', 'dalhousie'];
   
   // Step 1: If no city is selected yet, prompt the user to select one
